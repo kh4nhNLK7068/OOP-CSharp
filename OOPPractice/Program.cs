6ac4ba985bdb03456c912c333bb495131e5a7b60
kh4nhNLK7068 <khanhnguyennlk4198@gmail.com>
2025-10-17 10:14:13 +0700

    change device

--- OOPPractice/Program.cs
+++ OOPPractice/Program.cs
@@ -26,10 +26,10 @@ namespace OOPPractice
 
         static void Main(string[] args)
         {
-            /*Pig obj = new Pig();
+            Pig obj = new Pig();
             obj.animalSound();
             Level enumString = Level.Second;
-            Console.WriteLine($"{enumString}");*/
+            Console.WriteLine($"{enumString}");
 
             /*int[] numArr = { 1, 2, 3 };
             try
@@ -45,6 +45,24 @@ namespace OOPPractice
                 Console.WriteLine("Try again!");
             }*/
 
+            /*
+            Thứ 3 21/10/2025 deadline
+            + build public Webform (Không mã hóa - mã hóa một phần - toàn phần) + (Build project -> đưa lên IIS -> public domain web {Thực hành}) / App winfroms (install 2017, 2019(unnecessary), 2022 ver Visual Code xem cách build app 3 vers này + bugs thường gặp, tìm đọc thêm errors {Lý thuyết})
+            + Dataset (.xsd) trong .NET {Lý thuyết}
+
+            Thứ 5 23/10/2025 - Lý thuyết
+            + HTML/CSS/JS in .NET
+            + Telerik (UI/UX)
+            + DevExpress (dùng mỗi HRM)
+            + SAP Crystal Report (DÙNG NHIỀU, winforms & webform)
+
+
+            Học học:
+            Entity Framework / Dapper
+            Core (old 4,5,6) (new 8,9,..)
+            .NET Framework (4.0 -> 4.8 ver)
+            MVC + core / web API
+             */
             checkAge(15);
         }
     }
